a496153596372fdeacc43548328c5dc448adc452
Shahzeb khan <shahzeb.khan@sap.com>
2019-07-08 17:10:25 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,16 @@
 exports.config = {
     profile: "integration",    
     //TODO Replace with url of deployed Shopping Cart
-    baseUrl: "https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html"
+    baseUrl: "https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
+    reporters : [
+		{
+			name : './reporter/junitReporter',
+			reportName: './target/report/report.xml'
+		},
+		{
+			name : './reporter/screenshotReporter',
+			screenshotsRoot: './target/report'
+		}]
     //auth: {
     //  "sapcloud-form": {
     //    user: "${params.user}",
