4b4b82553a3e74052499eda52a322da483999c1e
Shahzeb khan <shahzeb.khan@sap.com>
2019-07-11 17:23:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+var webdriverio = require('webdriverio');
+var options = {
+    host: 'selenium',
+    port: 4444,
+    desiredCapabilities: {
+        browserName: 'chrome'
+    }
+};
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function(title) {
+        console.log('Title was: ' + title);
+    })
+    .end()
+    .catch(function(err) {
+        console.log(err);
+    });
